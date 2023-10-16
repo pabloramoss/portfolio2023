73adb91bb6479ae738920867f685fadf79168035
pabloramoss <pablo-362@hotmail.com>
2023-10-16 23:00:55 +0200

    fix: center text hero in mobile, delete divider in title

--- src/Components/sections/Hero.tsx
+++ src/Components/sections/Hero.tsx
@@ -7,15 +7,15 @@ export default function Hero() {
   return (
     <section className="flex flex-col md:flex-row gap-4 h-screen items-center" id="home">
       <div className="flex flex-col gap-4 h-full justify-center items-center md:items-start">
-        <div className="grid text-center md:text-start">
+        <div className="grid text-center place-items-center md:place-items-start md:text-start">
           <Reveal>
-            <h2 className="font-extrabold text-3xl">Hi, my name is</h2>
+            <h2 className="font-extrabold flex text-3xl">Hi, my name is</h2>
           </Reveal>
           <Reveal>
             <h1 className="font-extrabold text-7xl">Pablo Ramos</h1>
           </Reveal>
         </div>
-        <div className="grid text-center md:text-start">
+        <div className="grid text-center place-items-center md:place-items-start md:text-start">
           <Reveal>
             <h2 className="text-3xl">
               I am{' '}
